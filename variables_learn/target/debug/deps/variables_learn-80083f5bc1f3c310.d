584881e78e8697d28c7ece447e2d6dbb6b26f7d3
Manasa Manoj <manasamanojnbr1145@gmail.com>
2025-10-27 20:09:37 +0530

    Delete variables_learn/target directory

--- variables_learn/target/debug/deps/variables_learn-80083f5bc1f3c310.d
+++
@@ -1,5 +0,0 @@
-/home/manasa/Desktop/rust/variables_learn/target/debug/deps/variables_learn-80083f5bc1f3c310.d: src/main.rs
-
-/home/manasa/Desktop/rust/variables_learn/target/debug/deps/libvariables_learn-80083f5bc1f3c310.rmeta: src/main.rs
-
-src/main.rs:
