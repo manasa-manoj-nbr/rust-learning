976f9092acaa6d500c6fcd2f059f9daadc6557f2
Manasa Manoj <manasamanojnbr1145@gmail.com>
2025-10-27 20:10:07 +0530

    Delete hello_world/hello_cargo/target directory

--- hello_world/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d
+++
@@ -1,5 +0,0 @@
-/home/manasa/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d: src/main.rs
-
-/home/manasa/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18: src/main.rs
-
-src/main.rs:
--- hello_world/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/manasa/Desktop/rust/hello_cargo/target/debug/hello_cargo: /home/manasa/Desktop/rust/hello_cargo/src/main.rs
